83737d03b01c14487943d16fe3aec11a15a00689
Shreetej Hadge <shreetejhadge1308@gmail.com>
2025-01-30 14:13:33 -0700

    added a search bar and a few tabsto the sidebar and styled it

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './styles.css';
 import homePage from './homepage';
+import sidebar from './sidebar';
 
 console.log('heyyyyyyyyyyyy')
 
 homePage();
+sidebar();
--- src/sidebar.js
+++ src/sidebar.js
@@ -1,2 +1,31 @@
-const sidebar = document.querySelector('#sidebar');
-const searchBar = document.createElement('input');
+export default function sidebar() {
+    const sidebar = document.querySelector('#sidebar');
+    const sidebarList = document.createElement('div');
+    sidebarList.classList.add('sidebar-list');
+    sidebar.appendChild(sidebarList)
+    displaySearchBar();
+    sidebarItems();
+}
+
+function displaySearchBar() {
+    const sidebar = document.querySelector('#sidebar');
+    const sidebarList = document.querySelector('.sidebar-list');
+    const searchBar = document.createElement('input');
+    searchBar.type = 'text';
+    searchBar.id = 'search-bar';
+    searchBar.placeholder = 'Search';
+    sidebarList.appendChild(searchBar);
+    sidebar.appendChild(sidebarList);
+}
+
+function sidebarItems() {
+    const sidebarList = document.querySelector('.sidebar-list');
+    const sidebarItems = ['My Day', 'Important', 'Planned', 'Tasks', 'Completed', 'All'];
+    sidebarItems.forEach(item => {
+        const sidebarItem = document.createElement('div');
+        sidebarItem.classList.add('sidebar-item');
+        sidebarItem.textContent = item;
+        sidebarList.appendChild(sidebarItem);
+    });
+    sidebarList.appendChild(addTaskButton());
+}
