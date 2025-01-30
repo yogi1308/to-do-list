803c33833ed15b38f44ebb36f9e5ff2d04eae3bf
Shreetej Hadge <shreetejhadge1308@gmail.com>
2025-01-29 18:02:57 -0700

    added more basic files and added css reset and html boilerplate

---
+++ src/homepage.js
@@ -0,0 +1,3 @@
+export default function homePage() {
+    console.log('home page');
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log('HEyyyyyyyyyyyy')
+import './style.css';
+import homePage from './homePage';
+
+console.log('heyyyyyyyyyyyy')
